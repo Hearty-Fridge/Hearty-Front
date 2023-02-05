47d8f40f4347575864a9ce3ddc9b109e13512dd8
yexjin <tnska0421@naver.com>
2023-02-05 14:18:35 +0900

    Feat: Layout css 수정

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -18,8 +18,8 @@ const Layout = ({ children }) => {
 };
 
 const Wrapper = styled.div`
-  margin-left: 80px;
-  margin-right: 80px;
+  // header를 fixed 로 변경하여 header의 높이만큼 padding-top 부여
+  padding: 144px 80px 0;
 `;
 
 export default Layout;
