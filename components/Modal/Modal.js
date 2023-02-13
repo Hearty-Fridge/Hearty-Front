75e9b53a6d3e435a360880dab0b1a8d7148634d6
khyogeun_@naver.com <khyogeun_@naver.com>
2023-02-13 22:11:16 +0900

    Feat: Modal Container Using Portal(react-dom)

---
+++ components/Modal/Modal.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { useCallback, useEffect } from 'react';
+import ModalContainer from './ModalContainer';
+
+const Modal = ({ children, show, onCloseModal }) => {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  // 외부 스크롤을 막기 위해
+  useEffect(() => {
+    const $body = document.querySelector('body');
+    const overflow = $body.style.overflow;
+    $body.style.overflow = 'hidden';
+    return () => {
+      $body.style.overflow = overflow;
+    };
+  }, []);
+
+  if (!show) return null;
+  return (
+    <ModalContainer>
+      <Container onClick={onCloseModal}>
+        <Content onClick={stopPropagation}>{children}</Content>
+      </Container>
+    </ModalContainer>
+  );
+};
+
+export default Modal;
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: #000000cc;
+  z-index: 1001;
+`;
+
+const Content = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
