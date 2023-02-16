d8158b0acf7536de52d8f8b37a54f4f9cbaa85e0
yexjin <tnska0421@naver.com>
2023-02-16 20:54:13 +0900

    Style: navigation

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -19,7 +19,7 @@ const Layout = ({ children }) => {
 
 const Wrapper = styled.div`
   // header를 fixed 로 변경하여 header의 높이만큼 padding-top 부여
-  padding: 144px 80px 0;
+  padding: 86px 80px 0;
 `;
 
 export default Layout;
