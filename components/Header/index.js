231f702fc3074def43c520a712f8d76d1c3c8361
yexjin <tnska0421@naver.com>
2023-03-11 21:27:27 +0900

    Fix: logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -63,7 +63,7 @@ const Header = () => {
     <StyledHeader>
       <Navigation>
         <Link href="/">
-          <Image src="/image/HFogo.png" alt="logo" width={145} height={111} />
+          <Image src="/image/logo.png" alt="logo" width={145} height={145} />
         </Link>
         {NAV_MENU.map((navMenu) => (
           <NavLink
