cb384f492f3daf5a4f2873f12f3213b43006e431
khyogeun_@naver.com <khyogeun_@naver.com>
2023-03-24 21:11:41 +0900

    Fix: cannot reserve

--- pages/map.js
+++ pages/map.js
@@ -25,7 +25,6 @@ const MapPage = () => {
   const setVisibleListInBoundary = useCallback(() => {
     // 이 부분 업데이트 될 때마다 Ka , Va 저 부분 바뀌는거 같음
     // TODO : 버그 고치기
-    console.log(boundsRef.current);
     const minLat = boundsRef.current?.Va.lo;
     const maxLat = boundsRef.current?.Va.hi;
     const minLng = boundsRef.current?.Ga.lo;
